0b4b31c4095c949080aba1c39ec8ff9d261d9b38
Thomas Miller <thomas.miller147@gmail.com>
2023-04-27 10:18:41 +0200

    Youtube

--- src/components/Shots.tsx
+++ src/components/Shots.tsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import shotsData from "../data/shots.json";
 import { Shot } from "../types/shots";
 import { set } from "../store/shotSlice";
+import Video from "./Video";
 
 const shots: Shot[] = Object.values(shotsData).map((value: Shot) => ({
   name: value.name,
@@ -14,9 +15,11 @@ const Shots: React.FC = () => {
   const currentShot: Shot = useSelector((state: { shot: Shot }) => state.shot);
   return (
     <div className="flex-1">
-      <h2 className="mb-10 text-2xl font-bold">Shots</h2>
-      <div className="mb-10 text-xl font-bold">
-        <span className="text-primary-600">Shotname: {currentShot.name}</span>
+      <h2 className="mb-10 text-2xl font-bold">
+        Shots &gt; {currentShot.name}
+      </h2>
+      <div className="mx-auto mb-10 max-w-5xl text-xl font-bold">
+        <Video />
       </div>
       <div className="grid gap-y-4 md:grid-cols-3 md:gap-x-4 xl:grid-cols-4">
         {shots.map((shot, i) => (
--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,5 +1,29 @@
+import YouTube, { YouTubeProps } from "react-youtube";
+import { useSelector } from "react-redux";
+import { Shot } from "../types/shots";
+
 const Video = () => {
-  return <div></div>;
+  const currentShot: Shot = useSelector((state: { shot: Shot }) => state.shot);
+  const onPlayerReady = (event: {
+    target: { pauseVideo: () => void };
+  }): void => {
+    event.target.pauseVideo();
+  };
+  const opts: YouTubeProps["opts"] = {
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+  return (
+    <div className="youtubeContainer relative mb-12 h-0 w-full overflow-hidden pb-[56.25%]">
+      <YouTube
+        videoId={currentShot.videoID}
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        opts={opts}
+        onReady={onPlayerReady}
+      />
+    </div>
+  );
 };
 
 export default Video;
